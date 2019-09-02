33d56f300bea0dbb7b4b6cbf70d802979d86ba29
sanggwon <ck3929@gmail.com>
2019-09-02 14:27:44 +0900

    콘솔 찍어보기

--- 제 04강 Servlet 맛보기/src/com/javalec/ex/HelloWorld.java
+++ 제 04강 Servlet 맛보기/src/com/javalec/ex/HelloWorld.java
@@ -27,6 +27,7 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("helloworld");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
